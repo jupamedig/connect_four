2011cf3ae2ac6cfa8c4f249cd667f0c22bd7eda7
Juan Mendez & Jeremy Sabat <pair+jupamedig.velveetachef@devbootcamp.com>
2014-11-06 16:06:39 -0800

    refactor view

--- public/js/View.js
+++ public/js/View.js
@@ -1,8 +1,36 @@
+
 $(document).ready(function() {
-  $("button").on("click", function(e) {
-    var colClicked = e.currentTarget.name.slice(-1);
-    var color = "red";
-    var rowNum = addPiece(colClicked, color);
-    $("#col" + colClicked + " #" + rowNum).addClass(color);
-  })
+
+  var theView = new View();
+  theView.bindEventListeners();
 });
+
+function View() {
+  this.clickCount = 0;
+}
+
+View.prototype.bindEventListeners = function() {
+  var self = this;
+
+    $("button").on("click", function(e) {
+      var colClicked = e.currentTarget.name.slice(-1);
+      self.incrementClickCount();
+      self.addNextMove(colClicked);
+    })
+};
+
+View.prototype.addNextMove = function(colClicked) {
+  var rowNum = placePiece(colClicked, this.currentColor);
+  $piece = $('<p class="' + this.currentColor + '"></p>');
+  $('#col' + colClicked + ' #row0').innerHTML($piece);
+
+  // $("#col" + colClicked + " #row" + rowNum).addClass(this.currentColor);  ?? Needed?
+};
+
+View.prototype.currentColor = function() {
+  return (this.clickCount % 2 === 0) ?  "black" : "red"
+};
+
+View.prototype.incrementClickCount = function() {
+  this.clickCount += 1;
+};
